1388b3c6c6bb91ae45eb7f1e015994aae75fb7e7
Mohammad Sharif <mamadsh404@gmail.com>
2024-03-26 17:04:23 +0330

    create schema camera

---
+++ app/app/schemas/camera.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel, Field, ConfigDict
+
+
+class CameraBase(BaseModel):
+    is_active: Optional[bool] = True
+    camera_ip: Optional[str] = None
+    camera_code: Optional[str] = None
+    location: Optional[str] = None
+
+
+class CameraCreate(CameraBase):
+    camera_code: str = Field(..., min_length=1)
+    camera_ip: str = Field(...)
+    location: str = Field(...)
+    is_active: bool = Field(...)
+
+
+class CameraUpdate(CameraBase):
+    pass
+
+
+class CameraInDBBase(CameraBase):
+    id: int
+    created: datetime
+    modified: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Camera(CameraInDBBase):
+    pass
+
+
+class CameraInDB(CameraInDBBase):
+    pass
+
+
+class GetCamera(BaseModel):
+    items: List[Camera]
+    all_items_count: int

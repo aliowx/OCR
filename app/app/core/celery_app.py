c63d2b6145ab930af9d7f7c53f46e37d7b2345d2
Mohammad Sharif <mamadsh404@gmail.com>
2024-04-09 11:04:45 +0330

    add DB psql to celery

--- app/app/core/celery_app.py
+++ app/app/core/celery_app.py
@@ -1,8 +1,45 @@
-from celery import Celery
-
+from celery import Celery, Task
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.scoping import scoped_session
+from sqlalchemy.orm.session import sessionmaker
 from app.core.config import settings
 
 celery_app = Celery("worker", backend="rpc://", broker=str(settings.REDIS_URI))
 
+
+ScopedSession = None
+
+from celery.signals import worker_init
+
+
+@worker_init.connect
+def bootstrap(*args, **kwargs):
+    global ScopedSession
+    engine = create_engine(
+        settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True
+    )
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    ScopedSession = scoped_session(SessionLocal)
+
+
+class DatabaseTask(Task):
+    abstract = True
+
+    _session = None
+
+    def after_return(self, *args, **kwargs):
+        if self._session is not None:
+            self._session.remove()
+
+    @property
+    def session(self):
+        if self._session is None:
+            self._session = ScopedSession
+
+        return self._session
+
+
 celery_app.conf.task_routes = {"app.celery.worker.test_celery": "main-queue"}
+celery_app.conf.task_routes = {"app.celery.worker.add_plates": "main-queue"}
+celery_app.conf.task_routes = {"app.celery.worker.update_record": "main-queue"}
 celery_app.conf.update(task_track_started=True)

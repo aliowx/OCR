f9d653d7ea9a67da21e3ad901fa839db275b8773
Mohammad Sharif <mamadsh404@gmail.com>
2024-04-09 11:06:55 +0330

    add crud

--- app/app/crud/__init__.py
+++ app/app/crud/__init__.py
@@ -2,3 +2,5 @@ from .crud_user import user
 from .crud_camera import camera
 from .crud_parking import parking
 from .crud_image import image
+from .crud_plate import plate
+from .crud_record import record

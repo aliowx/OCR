16fb5936a18a6792f6fc9dc4ded8fd70dc813aa6
Mohammad Sharif <mamadsh404@gmail.com>
2024-04-27 16:53:48 +0330

    delete additional code

--- app/app/db/init_db.py
+++ app/app/db/init_db.py
@@ -32,57 +32,9 @@ def create_super_admin(db: Session) -> None:
     logger.info("first super user created")
 
 
-def add_location_date(db: Session) -> None:
-    if not crud.province.count(db):
-        try:
-            with open(
-                config.STATIC_DIR / "location/iran/provinces.json", "r"
-            ) as province_data:
-                provinces = json.load(province_data)
-                for p in provinces:
-                    crud.province.create(
-                        db,
-                        obj_in={
-                            "name_fa": p["name"],
-                            "pid": p["id"],
-                        },
-                        commit=False,
-                    )
-                db.commit()
-        except FileNotFoundError as e:
-            logger.error(
-                "Province data not found, add provinces.json file to app/static/location/iran"
-            )
-            raise e
-
-    if not crud.city.count(db):
-        try:
-            with open(
-                config.STATIC_DIR / "location/iran/cities.json", "r"
-            ) as city_data:
-                cities = json.load(city_data)
-                for c in cities:
-                    crud.city.create(
-                        db,
-                        obj_in={
-                            "name_fa": c["name"],
-                            "cid": c["id"],
-                            "province_id": c["province_id"],
-                        },
-                        commit=False,
-                    )
-                db.commit()
-        except FileNotFoundError as e:
-            logger.error(
-                "City data not found, add cities.json file to app/static/location/iran"
-            )
-            raise e
-
-
 def init_db(db: Session) -> None:
     try:
         create_super_admin(db)
-        add_location_date(db)
     except Exception as e:
         logger.error(f"initial data creation error\n{e}")
 

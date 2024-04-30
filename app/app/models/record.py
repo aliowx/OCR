d3f5699f0db675047b3323c74649f858f23abbbf
Mohammad Sharif <mamadsh404@gmail.com>
2024-04-30 12:37:28 +0330

    update model record to sqlalchemy2

--- app/app/models/record.py
+++ app/app/models/record.py
@@ -1,6 +1,4 @@
-from typing import TYPE_CHECKING
 from sqlalchemy import (
-    Column,
     ForeignKey,
     Integer,
     String,
@@ -8,40 +6,38 @@ from sqlalchemy import (
     Float,
 )
 from datetime import datetime
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from app.db.base_class import Base
-
-if TYPE_CHECKING:
-    from .user import User
-    from .image import Image  # noqa: F401
 
 
 class Record(Base):
-    id = Column(Integer, primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
 
-    ocr = Column(String, index=True)
+    ocr: Mapped[str] = mapped_column(String, index=True)
 
-    start_time = Column(
+    start_time: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), default=datetime.now, index=True
     )
-    end_time = Column(
+    end_time: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), default=datetime.now, index=True
     )
 
-    score = Column(Float, nullable=True, default=None, index=True)
+    score: Mapped[float] = mapped_column(
+        Float, nullable=True, default=None, index=True
+    )
 
-    plates = relationship("Plate", back_populates="record")
-
-    best_lpr_id = Column(
+    best_lpr_id: Mapped[int] = mapped_column(
         Integer,
         ForeignKey("image.id", onupdate="CASCADE", ondelete="SET NULL"),
         index=True,
     )
     best_lpr = relationship("Image", foreign_keys=best_lpr_id)
 
-    best_big_image_id = Column(
+    best_big_image_id: Mapped[int] = mapped_column(
         Integer,
         ForeignKey("image.id", onupdate="CASCADE", ondelete="SET NULL"),
         index=True,
     )
     best_big_image = relationship("Image", foreign_keys=best_big_image_id)
+
+    plates = relationship("Plate", back_populates="record")

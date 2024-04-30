fe0278cf2a5798c07eb083dbb400d23e82a852b2
Mohammad Sharif <mamadsh404@gmail.com>
2024-04-30 12:37:12 +0330

    update model plate to sqlalchemy2

--- app/app/models/plate.py
+++ app/app/models/plate.py
@@ -1,46 +1,45 @@
-from typing import TYPE_CHECKING
 from datetime import datetime
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from app.db.base_class import Base
-
-if TYPE_CHECKING:
-    from .user import User  # noqa: F401
-    from .camera import Camera  # noqa: F401
 
 
 class Plate(Base):
-    id = Column(Integer, primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
 
-    ocr = Column(String, index=True)
+    ocr: Mapped[str] = mapped_column(String, index=True)
 
-    lpr_id = Column(
+    record_time: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=datetime.now, index=True
+    )
+
+    number_line: Mapped[int] = mapped_column(Integer)
+
+    floor_number: Mapped[int] = mapped_column(Integer)
+    floor_name: Mapped[str] = mapped_column(String)
+
+    name_parking: Mapped[str] = mapped_column(String)
+
+    camera_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("camera.id"), index=True
+    )
+    camera_plate = relationship("Camera", back_populates="parking_plate")
+
+    record_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("record.id"), index=True
+    )
+    record = relationship("Record", back_populates="plates")
+
+    lpr_id: Mapped[int] = mapped_column(
         Integer,
         ForeignKey("image.id", onupdate="CASCADE", ondelete="SET NULL"),
         index=True,
     )
     lpr = relationship("Image", foreign_keys=lpr_id)
 
-    big_image_id = Column(
+    big_image_id: Mapped[int] = mapped_column(
         Integer,
         ForeignKey("image.id", onupdate="CASCADE", ondelete="SET NULL"),
         index=True,
     )
     big_image = relationship("Image", foreign_keys=big_image_id)
-
-    record_time = Column(
-        DateTime(timezone=True), default=datetime.now, index=True
-    )
-
-    number_line = Column(Integer)
-
-    floor_number = Column(Integer)
-    floor_name = Column(String)
-
-    name_parking = Column(String)
-
-    camera_id = Column(Integer, ForeignKey("camera.id"), index=True)
-    camera_plate = relationship("Camera", back_populates="parking_plate")
-
-    record_id = Column(Integer, ForeignKey("record.id"), index=True)
-    record = relationship("Record", back_populates="plates")

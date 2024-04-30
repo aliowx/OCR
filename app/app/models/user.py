6bba5ec88d6ebbb344bcdd162055dedddc3489cb
Mohammad Sharif <mamadsh404@gmail.com>
2024-04-30 12:37:38 +0330

    update model user to sqlalchemy2

--- app/app/models/user.py
+++ app/app/models/user.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.base_class import Base
 
 
 class User(Base):
-    id = Column(Integer, primary_key=True, index=True)
-    full_name = Column(String(50))
-    email = Column(String(50), index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean(), default=True)
-    is_superuser = Column(Boolean(), default=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    full_name: Mapped[str] = mapped_column(String(50), nullable=True)
+    email: Mapped[str] = mapped_column(String(50), index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), default=True)
+    is_superuser: Mapped[bool] = mapped_column(Boolean(), default=False)

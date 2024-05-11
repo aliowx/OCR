fc7929b27da645c7df99c7e43d9f20f4bbd19bd5
Mohammad Sharif <mamadsh404@gmail.com>
2024-05-11 10:41:44 +0330

    calculation price According to zone

--- app/app/api/api_v1/services/records_services.py
+++ app/app/api/api_v1/services/records_services.py
@@ -1,15 +1,12 @@
-from app import crud, models, schemas, utils
-from app import exceptions as exc
+from app import crud, schemas
 from sqlalchemy.ext.asyncio import AsyncSession
-from datetime import datetime
-from fastapi.encoders import jsonable_encoder
-from app.core.celery_app import celery_app
 
 
 async def calculator_price(db: AsyncSession, *, score, skip, limit, asc):
     records = await crud.record.find_records(
         db, input_score=score, skip=skip, limit=limit, asc=asc
     )
+
     weekdays = [
         "monday",
         "tuesday",
@@ -21,14 +18,23 @@ async def calculator_price(db: AsyncSession, *, score, skip, limit, asc):
     ]
     for t in range(len(records[0])):
         item_record = records[0][t]
-        price_weekly_day = weekdays[item_record.end_time.weekday()]
+
+        # Calculation of parking time
         item_record.parking_time = str(
             item_record.end_time - item_record.start_time
         )
+        # Calculation hours, conversion minutes and seconds to hours
         hours, minutes, seconds = map(int, item_record.parking_time.split(":"))
         total_hours = hours + minutes / 60 + seconds / 3600
-        item_record.parking_price = (
-            total_hours * item_record.price_model[price_weekly_day]
-        )
+
+        if item_record.price_model["price_type"] == "weekly":
+            price_weekly_day = weekdays[item_record.end_time.weekday()]
+            item_record.parking_price = (
+                total_hours * item_record.price_model[price_weekly_day]
+            )
+        if item_record.price_model["price_type"] == "zone":
+            item_record.parking_price = (
+                total_hours * item_record.price_model["price"]
+            )
 
     return schemas.GetRecords(items=records[0], all_items_count=records[1])

a1b1c10d1cde4b4a959c55e65bc2b8fc5c0dfbe0
Mohsen Barzegar <mohsenbarzegar@disroot.org>
2024-05-28 16:05:11 +0330

    refactor: remove custom APIRouter class

--- app/app/api/route.py
+++ app/app/api/route.py
@@ -1,6 +1,4 @@
-from typing import Any, Callable
-
-from fastapi import APIRouter as FastAPIRouter
+from fastapi import APIRouter
 
 from app.api.endpoints import (
     camera,
@@ -12,28 +10,6 @@ from app.api.endpoints import (
     users,
     utils,
 )
-
-
-class APIRouter(FastAPIRouter):
-    def add_api_route(
-        self,
-        path: str,
-        endpoint: Callable[..., Any],
-        *,
-        include_in_schema: bool = True,
-        **kwargs: Any
-    ) -> None:
-        if path.endswith("/"):
-            alternate_path = path[:-1]
-        else:
-            alternate_path = path + "/"
-        super().add_api_route(
-            alternate_path, endpoint, include_in_schema=False, **kwargs
-        )
-        return super().add_api_route(
-            path, endpoint, include_in_schema=include_in_schema, **kwargs
-        )
-
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/user", tags=["users"])

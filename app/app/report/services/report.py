061c1da591c7edee45d43d7fa098ccfe9ef8c2bf
Mohammad Sharif <mamadsh404@gmail.com>
2024-06-19 17:22:33 +0330

    service report zone

--- app/app/report/services/report.py
+++ app/app/report/services/report.py
@@ -1,29 +1,55 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from fastapi.encoders import jsonable_encoder
 from app.core.exceptions import ServiceFailure
 from app.parking.repo import parking_repo
-from app.parking.schemas import SetZonePriceInput
+from app.parking.schemas import Status
 from app.parking.services import parkingzone as parkingzone_services
-from app.parking.repo import parkingzone_repo, parkinglot_repo
+from app.report.repo import (
+    parkingzonereportrepository,
+    parkinglotreportrepository,
+)
 from app.utils import MessageCodes, PaginatedContent
-from app.report.schemas import ZoneLots
+from app.report.schemas import ZoneLots, ReadZoneLotsParams
 
 
-async def report_zone(db: AsyncSession):
+async def report_zone(db: AsyncSession, params: ReadZoneLotsParams):
+
     # list all zone
-    parkingzones = await parkingzone_repo.get_multi(db, limit=None)
+    parkingzones = await parkingzonereportrepository.get_multi_by_filter(
+        db, params=params
+    )
 
-    # list lots zone
     list_lots_zone = []
-
     for zone in parkingzones:
-        lots = await parkinglot_repo.find_lines(
-            db, limit=None, input_zone_id=zone.id
+        # list lots zone
+        lots = await parkinglotreportrepository.find_lines(
+            db, input_zone_id=zone.id, params=params
         )
         if lots:
-            ZoneLots.zone_name = zone.name
-            ZoneLots.list_lots = lots
-            list_lots_zone.append(ZoneLots)
-    
+            capacity_empty = capacity = len(lots)
+            lots = jsonable_encoder(lots)
+            for lot in lots:
+                if lot["status"] == Status.full:
+                    capacity_empty = capacity - 1
+            zone_lots = ZoneLots(
+                zone_name=zone.name,
+                list_lots=lots,
+                capacity=capacity,
+                capacity_empty=capacity_empty,
+            )
+            list_lots_zone.append(jsonable_encoder(zone_lots))
+    if params.size is not None:  # limit
+        list_lots_zone = list_lots_zone[: params.size]
 
-    return
+    if params.page is not None:  # skip
+        list_lots_zone = list_lots_zone[:: params.page]
+
+    if params.asc:
+        list_lots_zone.reverse()
+
+    return PaginatedContent(
+        data=list_lots_zone,
+        total_count=len(list_lots_zone),
+        size=params.size,
+        page=params.page,
+    )

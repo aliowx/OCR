066b149a67fb3e0cd905257341ee578e707f1f02
Mohammad Sharif <mamadsh404@gmail.com>
2024-07-03 17:37:53 +0330

    add image_id to schemas quipment

--- app/app/parking/schemas/equipment.py
+++ app/app/parking/schemas/equipment.py
@@ -12,6 +12,7 @@ class EquipmentBase(BaseModel):
     serial_number: str | None = None
     ip_address: str | None = None
     zone_id: int | None = None
+    image_id: int | None = None
     additional_data: dict | None = None
 
 
@@ -94,60 +95,3 @@ class ReadEquipmentsParams(BaseModel):
         if self.end_date:
             filters.created__lte = self.end_date
         return filters
-    
-# # for camera
-# from datetime import datetime
-# from typing import List, Optional
-
-# from pydantic import BaseModel, ConfigDict, Field
-
-
-# class CameraBase(BaseModel):
-#     is_active: Optional[bool] = True
-#     camera_ip: Optional[str] = None
-#     camera_code: Optional[str] = None
-#     location: Optional[str] = None
-#     image_id: Optional[int] = None
-#     zone_id: Optional[int] = None
-
-
-# class CameraCreate(CameraBase):
-#     camera_code: str = Field(..., min_length=1)
-#     camera_ip: str = Field(...)
-#     location: str = Field(...)
-#     zone_id: str = Field(...)
-#     is_active: bool = Field(...)
-
-
-# class CameraUpdate(CameraBase):
-#     pass
-
-
-# class CameraInDBBase(CameraBase):
-#     id: int
-#     created: datetime
-#     modified: datetime
-
-#     model_config = ConfigDict(from_attributes=True)
-
-
-# class Camera(CameraInDBBase):
-#     pass
-
-
-# class CameraInDB(CameraInDBBase):
-#     pass
-
-
-# class ParamsCamera(BaseModel):
-#     input_camera_code: str | None = None
-#     input_camera_ip: str | None = None
-#     input_location: str | None = None
-#     skip: int | None = 0
-#     limit: int | None = 100
-
-
-# class GetCamera(BaseModel):
-#     items: List[Camera]
-#     all_items_count: int
-

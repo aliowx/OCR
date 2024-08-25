bafb2b0ec3fc62abc8b66ab5a51e3b425b45a748
Mohammad Sharif <mamadsh404@gmail.com>
2024-08-25 18:15:32 +0330

    update mdoe payment

--- app/app/payment/models/payment.py
+++ app/app/payment/models/payment.py
@@ -1,22 +1,20 @@
-from sqlalchemy import Integer, String, DateTime, Float, ForeignKey
+from sqlalchemy import Integer, String, Float
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.db.base_class import Base, get_now_datetime_utc
+from app.db.base_class import Base
 
 
 class Payment(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
 
-    bill_id: Mapped[int] = mapped_column(
-        Integer,
-        ForeignKey("bill.id", onupdate="CASCADE", ondelete="SET NULL"),
-        nullable=True,
-        index=True,
-    )
-    bill_rel = relationship("Bill", foreign_keys=bill_id)
+    price: Mapped[float] = mapped_column(Float, nullable=True, index=True)
 
     status: Mapped[str] = mapped_column(String, nullable=True, index=True)
 
     tracking_code: Mapped[int] = mapped_column(
         Integer, nullable=True, index=True
+    )
+
+    payment_bill = relationship(
+        "PaymentBill", back_populates="payment_rel", lazy="immediate"
     )

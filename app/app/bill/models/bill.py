c76d1cda9cbc483e209ea081f10fa7644367a7b1
Mohammad Sharif <mamadsh404@gmail.com>
2024-08-25 18:20:36 +0330

    model paymentbill

--- app/app/bill/models/bill.py
+++ app/app/bill/models/bill.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Integer, String, DateTime, Float
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, DateTime, Float, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.base_class import Base, get_now_datetime_utc
-from datetime import datetime, UTC
+from datetime import datetime
 
 
 class Bill(Base):
@@ -25,3 +25,27 @@ class Bill(Base):
     price: Mapped[float] = mapped_column(Float, nullable=True, index=True)
 
     issued_by: Mapped[str] = mapped_column(String, nullable=True, index=True)
+
+    bill_payment = relationship(
+        "PaymentBill", back_populates="bill_rel", lazy="immediate"
+    )
+
+
+class PaymentBill(Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+
+    bill_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("bill.id", onupdate="CASCADE", ondelete="SET NULL"),
+        nullable=True,
+        index=True,
+    )
+    bill_rel = relationship("Bill", back_populates="bill_payment")
+
+    payment_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("payment.id", ondelete="SET NULL", onupdate="CASCADE"),
+        index=True,
+        nullable=True,
+    )
+    payment_rel = relationship("Payment", back_populates="payment_bill")

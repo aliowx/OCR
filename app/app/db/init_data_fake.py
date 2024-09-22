9087f7b794f57bf0124ce45957e545638a41c1e6
Mohammad Sharif <mamadsh404@gmail.com>
2024-09-22 12:41:46 +0330

    sync to async initial fake data

--- app/app/db/init_data_fake.py
+++ app/app/db/init_data_fake.py
@@ -1,10 +1,13 @@
 import logging
 
 from sqlalchemy.orm import Session
+from sqlalchemy import select, text
+from sqlalchemy.ext.asyncio import AsyncSession
 from app import crud
 from app import models
 from app.core.fake_data import fake_data
 from app.db.session import SessionLocal
+from app.db.session import AsyncSessionLocal
 from app import schemas
 import random
 import string
@@ -14,18 +17,19 @@ from cache.redis import redis_client
 import rapidjson
 from datetime import datetime, timedelta, timezone
 import time
+import asyncio
 
-from app.bill.services.bill import calculate_price
+from app.bill.services.bill import calculate_price_async
 from app.bill.schemas import bill as billSchemas
 
 
 logger = logging.getLogger(__name__)
 
 
-def commit_to_db(db: Session, data: any, name: str):
+async def commit_to_db(db: AsyncSession, data: any, name: str):
     db.add(data)
-    db.commit()
-    db.refresh(data)
+    await db.commit()
+    await db.refresh(data)
 
     logger.info(f"create {name}")
 
@@ -38,113 +42,146 @@ def generate_random_string(length):
     return random_string
 
 
-def create_parking(db: Session) -> None:
-    parking = db.query(models.Parking).first()
+async def create_parking(db: AsyncSession):
+    parking = await db.execute(select(models.Parking).limit(1))
 
     if not parking:
         parking = fake_data.PARKING
-        commit_to_db(db, data=parking, name="parking")
+        await commit_to_db(db, data=parking, name="parking")
     return parking
 
 
-def create_equipment(db: Session):
-    equipment_entrance = (
-        db.query(models.Equipment)
-        .where(
+async def create_equipment(db: AsyncSession):
+    # Asynchronous query for entrance equipment
+    equipment_entrance = await db.execute(
+        select(models.Equipment).where(
             models.Equipment.is_deleted == False,
             models.Equipment.equipment_type == 1,
         )
-        .all()
     )
-    equipment_exit = (
-        db.query(models.Equipment)
-        .where(
+    # Asynchronous query for exit equipment
+    equipment_exit = await db.execute(
+        select(models.Equipment).where(
             models.Equipment.is_deleted == False,
             models.Equipment.equipment_type == 2,
         )
-        .all()
     )
+
+    # Extract scalars (the actual records) from the query results
+    equipment_entrance = equipment_entrance.scalars().all()
+    equipment_exit = equipment_exit.scalars().all()
+
+    # Combine entrance and exit equipment into a single list
     equipment = []
     for entrance, exit in zip(equipment_entrance, equipment_exit):
         equipment.append(entrance.id)
         equipment.append(exit.id)
+
     return equipment
 
 
-def create_image(db: Session):
-    image = db.query(models.Image).first()
+async def create_image(db: AsyncSession):
+    image = await db.execute(select(models.Image).limit(1))
+    image = image.scalars().first()
+
     if not image:
         image = fake_data.IMAGE
-        return commit_to_db(db, data=image, name="image")
+        await commit_to_db(db, data=image, name="image")
+
     return image
 
 
-def create_price(db: Session):
-    price = (
-        db.query(models.Price)
-        .filter(
-            *[models.Price.hourly_fee == 0, models.Price.entrance_fee == 20000]
+async def create_price(db: AsyncSession):
+    price = await db.execute(
+        select(models.Price).filter(
+            models.Price.hourly_fee == 0, models.Price.entrance_fee == 20000
         )
-        .first()
     )
+    price = price.scalars().first()
+    print(price)
     if not price:
-        return commit_to_db(
+        await commit_to_db(
             db,
             data=models.Price(
                 name="first simple price", entrance_fee=20000, hourly_fee=0
             ),
-            name="image",
+            name="price",
         )
+
     return price
 
 
-def create_zone(db: Session):
-    zones = db.query(models.Zone).all()
+async def create_zone(db: AsyncSession):
+    zones = await db.execute(select(models.Zone))
+    zones = zones.scalars().all()
+
     zone_ids = []
-    price = create_price(db)
+    price = await create_price(db)
+
     for zone in zones:
         if not zone.price_id:
             zone.price_id = price.id
         zone_ids.append(zone.id)
+
     if not zones:
         zone = fake_data.ZONE
-        zone.zone_id = price.id
-        return commit_to_db(db, data=zone, name="zone")
+        zone.price_id = price.id
+        return await commit_to_db(db, data=zone, name="zone")
+
     return zone_ids
 
 
-def create_sub_zone(db: Session):
-    sub_zone = (
-        db.query(models.Zone)
-        .where(
+async def create_sub_zone(db: AsyncSession):
+    sub_zone = await db.execute(
+        select(models.Zone).where(
             models.Zone.is_deleted == False,
             models.Zone.name == fake_data.SUB_ZONE.name,
         )
-        .first()
     )
+    sub_zone = sub_zone.scalars().first()
+
     if not sub_zone:
         sub_zone = fake_data.SUB_ZONE
-        sub_zone.parent_id = create_zone(db).id
-        commit_to_db(db, data=sub_zone, name="sub_zone")
+        parent_zone = await create_zone(db)
+        sub_zone.parent_id = parent_zone[0]  # Assuming first zone ID is used
+        await commit_to_db(db, data=sub_zone, name="sub_zone")
+
     return sub_zone
 
 
-def create_records(db: Session):
+async def create_records(db: AsyncSession):
     records_data = [
         fake_data.RECORD1,
         fake_data.RECORD2,
     ]
-    zone_ids = create_zone(db)
-    image = create_image(db)
-    for record in records_data:
-        record.img_exit_id = image.id
-        record.img_entrance_id = image.id
-        record.zone_id = random.choice(zone_ids)
-        db.add(record)
-    db.commit()
-    db.refresh(record)
+    zone_ids = await create_zone(db)
+    image = await create_image(db)
+    latest_id = await latest_id_records(db)
+    if latest_id is None:
+        last_record = None
+        for record in records_data:
+            record.id = latest_id + 1
+            record.img_exit_id = image.id
+            record.img_entrance_id = image.id
+            record.zone_id = random.choice(zone_ids)
+            db.add(record)
+            last_record = record  # Store the last added record
+    else:
+        last_record = None
+        for record in records_data:
+            record.img_exit_id = image.id
+            record.img_entrance_id = image.id
+            record.zone_id = random.choice(zone_ids)
+            db.add(record)
+            last_record = record  # Store the last added record
+
+    await db.commit()
+
+    if last_record:  # Refresh only if there's a last record
+        await db.refresh(last_record)
+
     logger.info("create record")
-    create_records_past(db)
+    await create_records_past(db)
 
 
 list_status_record = [
@@ -159,19 +196,31 @@ status_bills = [
 ]
 
 
-def create_records_past(db: Session):
-    image = create_image(db)
-    zone_ids = create_zone(db)
-    latest_id_record = (
-        db.query(models.Record).order_by(models.Record.id.desc()).first()
-    ).id
+async def latest_id_records(db: AsyncSession):
+    # Asynchronous query to get the latest record ID
+    latest_record_query = (
+        select(models.Record).order_by(models.Record.id.desc()).limit(1)
+    )
+    latest_id_result = await db.execute(latest_record_query)
+    latest_id_record = latest_id_result.scalars().first()
+    if latest_id_record is None:
+        return None
+    return latest_id_record.id
+
+
+async def create_records_past(db: AsyncSession):
+    # Await asynchronous calls to create_image and create_zone
+    image = await create_image(db)
+    zone_ids = await create_zone(db)
+    latest_id = await latest_id_records(db)
+
     for i in range(1, 1000):
+        time = datetime.now(timezone.utc).replace(tzinfo=None)
         record = models.Record(
-            id=latest_id_record + i,
+            id=latest_id + i,
             plate=f"{random.randint(10,99)}{random.randint(10,70)}{random.randint(100,999)}{random.randint(10,99)}",
-            start_time=datetime.now(timezone.utc).replace(tzinfo=None),
-            end_time=datetime.now(timezone.utc).replace(tzinfo=None)
-            + timedelta(hours=random.randint(1, 23)),
+            start_time=time + timedelta(minutes=random.randint(1, 25)),
+            end_time=time + timedelta(hours=random.randint(1, 23)),
             img_entrance_id=None,
             img_exit_id=None,
             score=0.01,
@@ -185,15 +234,18 @@ def create_records_past(db: Session):
         )
         record.img_entrance_id = image.id
         record.img_exit_id = image.id
+
+        # Add record to the session
         db.add(record)
 
+        # If the record is finished, create a bill
         if record.latest_status == schemas.StatusRecord.finished:
             bill = models.Bill(
                 plate=record.plate,
                 start_time=record.start_time,
                 end_time=record.end_time,
                 issued_by=billSchemas.Issued.exit_camera.value,
-                price=calculate_price(
+                price=await calculate_price_async(
                     db,
                     zone_id=record.zone_id,
                     start_time_in=record.start_time,
@@ -205,11 +257,16 @@ def create_records_past(db: Session):
             )
             db.add(bill)
 
+        # Publish the record data to Redis
         redis_client.publish(
             "records:1", rapidjson.dumps(jsonable_encoder(record))
         )
-        crud.record._commit_refresh(db=db, db_obj=record, commit=False)
-    db.commit()
+
+        # Use async commit/refresh
+        await crud.record._commit_refresh(db=db, db_obj=record, commit=False)
+
+    # Commit all changes after the loop
+    await db.commit()
 
 
 type_camera_event = [
@@ -218,20 +275,20 @@ type_camera_event = [
 ]
 
 
-def create_events(db: Session):
-    image = create_image(db)
-    cameras = create_equipment(db)
-    zone_ids = create_zone(db)
+async def create_events(db: AsyncSession):
+    # Await asynchronous calls to create_image and create_equipment
+    image = await create_image(db)
+    cameras = await create_equipment(db)
+    zone_ids = await create_zone(db)
     events = []
+
     for _ in range(1, 50):
         event = models.Event(
-            plate=f"{random.randint(10,99)}{random.randint(10,70)}{random.randint(100,999)}{random.randint(10,99)}",
-            record_time=(
-                datetime(
-                    year=random.randint(2023, 2024),
-                    month=random.randint(1, 9),
-                    day=random.randint(1, 28),
-                )
+            plate=f"{random.randint(10, 99)}{random.randint(10, 70)}{random.randint(100, 999)}{random.randint(10, 99)}",
+            record_time=datetime(
+                year=random.randint(2023, 2024),
+                month=random.randint(1, 9),
+                day=random.randint(1, 28),
             ),
             plate_image_id=image.id,
             lpr_image_id=image.id,
@@ -240,12 +297,15 @@ def create_events(db: Session):
             type_camera=schemas.event.TypeCamera.entranceDoor.value,
         )
         events.append(event)
+        # Sending tasks to Celery asynchronously
         celery_app.send_task(
             "add_events",
             args=[jsonable_encoder(event)],
         )
 
-    time.sleep(3)
+    # Use asyncio.sleep instead of time.sleep to avoid blocking
+    await asyncio.sleep(3)
+
     for one_event in events:
         event = models.Event(
             plate=one_event.plate,
@@ -257,26 +317,30 @@ def create_events(db: Session):
             camera_id=one_event.camera_id,
             zone_id=one_event.zone_id,
         )
+        # Sending tasks to Celery asynchronously
         celery_app.send_task(
             "add_events",
             args=[jsonable_encoder(event)],
         )
 
 
-def init_db_fake_data(db: Session) -> None:
+async def init_db_fake_data(db: AsyncSession) -> None:
     try:
-        # create_parking(db)
-        # create_equipment(db)
-        # create_image(db)
-        # create_zone(db)
-        # create_sub_zone(db)
-        create_records(db)
-        # create_records_past(db)
-        create_events(db)
+        # await create_parking(db)
+        # await create_equipment(db)
+        # await create_image(db)
+        # await create_zone(db)
+        # await create_sub_zone(db)
+        await create_records(db)
+        # await create_records_past(db)
+        await create_events(db)
     except Exception as e:
-        logger.error(f"initial data creation error\n{e}")
+        await logger.error(f"initial data creation error\n{e}")
+
+    finally:
+        await db.close()
 
 
 if __name__ == "__main__":
-    db = SessionLocal()
-    init_db_fake_data(db)
+    db = AsyncSessionLocal()
+    asyncio.run(init_db_fake_data())

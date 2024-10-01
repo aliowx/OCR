aebe786a67c3cffa31fa75ea16a3aea49815b46f
Mohammad Sharif <mamadsh404@gmail.com>
2024-10-01 11:27:29 +0330

    return tag in get record

--- app/app/crud/crud_record.py
+++ app/app/crud/crud_record.py
@@ -204,8 +204,8 @@ class CRUDRecord(CRUDBase[Record, RecordCreate, RecordUpdate]):
                 Record,
                 ((Record.end_time) - (Record.start_time)).label("time_park"),
                 Zone.name,
-                equipment_entance.serial_number.label("camera_entrance"),
-                equipment_exit.serial_number.label("camera_exit"),
+                equipment_entance.tag.label("camera_entrance"),
+                equipment_exit.tag.label("camera_exit"),
             )
             .join(Zone, Record.zone_id == Zone.id)
             .join(img_entrance, Record.img_entrance_id == img_entrance.id)

35a357490a36317471dba6281cec53613a0baf49
Mohammad Sharif <mamadsh404@gmail.com>
2024-10-28 14:41:58 +0330

    fix

--- app/app/ticket/services/ticket.py
+++ app/app/ticket/services/ticket.py
@@ -17,7 +17,7 @@ async def get_record_bill(db: AsyncSession, *, params: ParamsTicket):
     for ticket in tickets:
         ticket[0].record = ticket[1]
         ticket[0].bill = ticket[2]
-        if ticket[2].record_id is not None:
+        if ticket[2] is not None and ticket[2].record_id is not None:
             ticket[0].record = await crud.record.get(
                 db, id=ticket[2].record_id
             )

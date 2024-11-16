e5db62f966add716ade84a3a74dd8672c150de9c
Mohammad Sharif <mamadsh404@gmail.com>
2024-11-16 12:28:22 +0330

    fix return plate into notifications

--- app/app/notifications/repo.py
+++ app/app/notifications/repo.py
@@ -4,16 +4,25 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 from app.crud.base import CRUDBase
 from .models.notification import Notifications
-from .schemas import NotificationsCreate, NotificationsUpdate, ParamsNotifications
+from .schemas import (
+    NotificationsCreate,
+    NotificationsUpdate,
+    ParamsNotifications,
+)
 import re
+from app.plate.models import PlateList
 
 
-class CRUDNotifications(CRUDBase[Notifications, NotificationsCreate, NotificationsUpdate]):
+class CRUDNotifications(
+    CRUDBase[Notifications, NotificationsCreate, NotificationsUpdate]
+):
     async def get_multi_by_filter(
         self, db: Session | AsyncSession, *, params: ParamsNotifications
     ) -> list[Notifications] | Awaitable[list[Notifications]]:
 
-        query = select(Notifications)
+        query = select(Notifications, PlateList).outerjoin(
+            PlateList, Notifications.plate_list_id == PlateList.id
+        )
 
         filters = [Notifications.is_deleted == False]
 
@@ -22,23 +31,24 @@ class CRUDNotifications(CRUDBase[Notifications, NotificationsCreate, Notificatio
 
         total_count = await self.count_by_filter(db, filters=filters)
 
-        order_by = Notifications.id.asc() if params.asc else Notifications.id.desc()
+        order_by = (
+            Notifications.id.asc() if params.asc else Notifications.id.desc()
+        )
 
         if params.size is None:
-            resualt = await self._all(
-                db.scalars(query.filter(*filters).order_by(order_by))
-            )
+            resualt = (
+                await db.execute(query.filter(*filters).order_by(order_by))
+            ).fetchall()
             return resualt, total_count
-        resualt = await self._all(
-            db.scalars(
+        resualt = (
+            await db.execute(
                 query.filter(*filters)
                 .offset(params.skip)
                 .limit(params.size)
                 .order_by(order_by)
             )
-        )
+        ).fetchall()
         return resualt, total_count
-
 
 
 notifications_repo = CRUDNotifications(Notifications)

16e17f930702ad83b7e1f873c0f633dc1d8e4b30
alialibakhshi <alialibakhshi9898@gmail.com>
2024-12-19 06:13:40 +0000

    clean code

--- app/tests/api/api_v1/test_bill.py
+++ app/tests/api/api_v1/test_bill.py
@@ -9,14 +9,10 @@ transport = ASGITransport(app=app)
 client = AsyncClient(transport=transport, base_url="http://test")
 
 
-
 @pytest.mark.asyncio
 class TestBill:
     async def test_bill_create(
-        self,
-        create_zone: dict,
-        create_record: dict,
-        create_bill: dict
+        self, create_zone: dict, create_record: dict, create_bill: dict
     ):
 
         assert "id" in create_bill
@@ -26,4 +22,35 @@ class TestBill:
         assert create_bill["record_id"] == create_record["id"]
         assert create_bill["zone_id"] == create_zone["id"]
 
+
+    async def test_bill_retrieve(
+        self, create_bill: dict  
+    ):
+        response = await client.get(f"{create_bill['id']}/")
+        assert response.status_code == 200
+        data = response.json()
+        
+        assert data['id'] == create_bill['id']
+        assert data['record_id'] == create_bill['record_id']
+        assert data['zone_id'] == create_bill['zone_id']
+        
+        
+    async def test_bill_update(
+        self, create_zone: dict, create_record: dict, create_bill: dict
+    ):
+        update_data =  {"zone_id": create_zone["id"], "record_id": create_record["id"]}
+        
+        response = await client.put(
+            f"{create_bill['id']}/update_bills",
+            json=update_data
+        )
+        
+        assert response.status_code == 200
+        updated_bill = response.json()
+        
+        
+        assert updated_bill['id'] == create_bill['id']
+        assert updated_bill['zone_id'] == create_zone['id']
+        assert updated_bill['record_id'] == create_record['id']
+        
     

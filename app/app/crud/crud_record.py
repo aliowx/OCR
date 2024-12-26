abaeaaed7fcd42cbc380b5e57a62af207a11337b
alialibakhshi <alialibakhshi9898@gmail.com>
2024-12-26 07:45:30 +0000

    chore: clean up and refactor CRUD query logic

--- app/app/crud/crud_record.py
+++ app/app/crud/crud_record.py
@@ -287,199 +287,67 @@ class CRUDRecord(CRUDBase[Record, RecordCreate, RecordUpdate]):
         db: Session | AsyncSession,
         *,
         params: schemas.ParamsRecord,
-        input_status_record: Optional[
-            List[schemas.record.StatusRecord]
-        ] = None,
+        input_status_record: Optional[List[schemas.record.StatusRecord]] = None,
         input_camera_entrance_id: Optional[list[int]] = None,
         input_camera_exit_id: Optional[list[int]] = None,
     ) -> list[Record] | Awaitable[list[Record]]:
 
         equipment_entance = aliased(Equipment)
         equipment_exit = aliased(Equipment)
-        query = (
-            select(
-                Record,
-                ((Record.end_time) - (Record.start_time)).label("time_park"),
-                Zone.name,
-                equipment_entance.tag.label("camera_entrance"),
-                equipment_exit.tag.label("camera_exit"),
-            )
-            .outerjoin(Zone, Record.zone_id == Zone.id)
-            .outerjoin(
-                equipment_entance,
-                Record.camera_entrance_id == equipment_entance.id,
-            )
-            .outerjoin(
-                equipment_exit, Record.camera_exit_id == equipment_exit.id
-            )
-        )
+
 
         filters = [Record.is_deleted == False]
+        
+        
+        def add_filter(condition, filter_expr):
+            if condition:
+                filters.append(filter_expr)
 
-        # if jalali_date is not None:
-        #     column_date_jalali_for_entrance = select(
-        #         Record.id,
-        #         func.format_jalali(Record.start_time, False).label(
-        #             "date_jalali_entrance"
-        #         ),
-        #     ).subquery()
-        #     column_date_jalali_for_exit = select(
-        #         Record.id,
-        #         func.format_jalali(Record.end_time, False).label(
-        #             "date_jalali_exit"
-        #         ),
-        #     ).subquery()
-        #     dj_entrance = aliased(column_date_jalali_for_entrance)
-        #     dj_exit = aliased(column_date_jalali_for_exit)
-        #     query = query.join(dj_entrance, Record.id == dj_entrance.c.id)
-        #     query = query.join(dj_exit, Record.id == dj_exit.c.id)
-        #     if jalali_date.in_start_entrance_jalali_date is not None:
-        #         filters.append(
-        #             dj_entrance.c.date_jalali_entrance
-        #             >= jalali_date.in_start_entrance_jalali_date,
-        #         )
-        #     if jalali_date.in_end_entrance_jalali_date is not None:
-        #         filters.append(
-        #             dj_entrance.c.date_jalali_entrance
-        #             <= jalali_date.in_end_entrance_jalali_date,
-        #         )
-        #
-        #     if jalali_date.in_start_exit_jalali_date is not None:
-        #         filters.append(
-        #             dj_exit.c.date_jalali_exit
-        #             >= jalali_date.in_start_exit_jalali_date,
-        #         )
-        #     if jalali_date.in_end_exit_jalali_date is not None:
-        #         filters.append(
-        #             dj_exit.c.date_jalali_exit
-        #             <= jalali_date.in_end_exit_jalali_date,
-        #         )
+        add_filter(params.input_plate and re.fullmatch(r"[0-9?]{9}", params.input_plate),
+               Record.plate.like(params.input_plate.replace("?", "_")))
 
-        if params.input_plate is not None and bool(
-            re.fullmatch(r"[0-9?]{9}", params.input_plate)
-        ):
-            value_plate = params.input_plate.replace("?", "_")
-            filters.append(Record.plate.like(value_plate))
+        add_filter(input_camera_exit_id and input_camera_exit_id != [0],
+                    Record.camera_exit_id.in_(input_camera_exit_id))
+        add_filter(input_camera_exit_id == [0],
+                    Record.camera_exit_id.is_(None))
+        add_filter(params.input_id,
+                    Record.id == params.input_id)
+        add_filter(params.input_score,
+                   Record.score >= params.input_score)
 
-        if input_camera_exit_id is not None and input_camera_exit_id != [0]:
-            filters.append(Record.camera_exit_id.in_(input_camera_exit_id))
+        query = (
+        select(
+            Record,
+            ((Record.end_time) - (Record.start_time)).label("time_park"),
+            Zone.name,
+            equipment_entance.tag.label("camera_entrance"),
+            equipment_exit.tag.label("camera_exit"),
+        )
+        .outerjoin(Zone, Record.zone_id == Zone.id)
+        .outerjoin(equipment_entance, Record.camera_entrance_id == equipment_entance.id)
+        .outerjoin(equipment_exit, Record.camera_exit_id == equipment_exit.id)
+        .filter(*filters)
+    )
 
-        if input_camera_exit_id == [0]:
-            filters.append(Record.camera_exit_id.is_(None))
-
-        if params.similar_plate is not None:
-            # Adjust similarity threshold if necessary
-            await db.execute(text("SET pg_trgm.similarity_threshold = 0.5"))
-            filters.append(text(f"plate % :similar_plate"))
-
-        if (
-            input_camera_entrance_id is not None
-            and input_camera_entrance_id != [0]
-        ):
-            filters.append(
-                Record.camera_entrance_id.in_(input_camera_entrance_id)
-            )
-
-        if input_camera_entrance_id == [0]:
-            filters.append(Record.camera_entrance_id.is_(None))
-
-        if params.input_id is not None:
-            filters.append(Record.id == params.input_id)
-
-        if params.input_time_park_less_than_min is not None:
-
-            filters.append(
-                func.extract("epoch", Record.end_time)
-                - (func.extract("epoch", Record.start_time))
-                < params.input_time_park_less_than_min / 60
-            )
-
-        if params.input_time_park_greater_than_min is not None:
-            filters.append(
-                func.extract("epoch", Record.end_time)
-                - (func.extract("epoch", Record.start_time))
-                > params.input_time_park_greater_than_min / 60
-            )
-
-        if params.input_zone_id is not None:
-            filters.append(Record.zone_id == params.input_zone_id)
-
-        if params.input_entrance_start_time is not None:
-            filters.append(
-                Record.start_time >= params.input_entrance_start_time
-            )
-
-        if params.input_entrance_end_time is not None:
-            filters.append(Record.start_time <= params.input_entrance_end_time)
-
-        if params.input_exit_start_time is not None:
-            filters.append(Record.end_time >= params.input_exit_start_time)
-
-        if params.input_exit_end_time is not None:
-            filters.append(Record.end_time <= params.input_exit_end_time)
-
-        if input_status_record is not None:
-            filters.append(Record.latest_status.in_(input_status_record))
-
-        if params.input_score is not None:
-            filters.append(Record.score >= params.input_score)
-
-        order_by = Record.id
-        if params.sort_by == schemas.record.SortBy.entrance_time:
-            order_by = Record.start_time
-
-        if params.sort_by == schemas.record.SortBy.exit_time:
-            order_by = Record.end_time
-
-        if (
-            params.input_entrance_persent_time is not None
-            and params.input_exit_persent_time is not None
-        ):
-            filters.append(
-                Record.end_time >= params.input_entrance_persent_time
-            )
-            filters.append(Record.start_time <= params.input_exit_persent_time)
 
         all_items_count = await db.scalar(
-            query.filter(*filters).with_only_columns(func.count()),
-            (
-                {}
-                if params.similar_plate is None
-                else {"similar_plate": params.similar_plate}
-            ),
+            query.with_only_columns(func.count()),
         )
-        if params.limit is None:
-            result = (
-                await db.execute(
-                    query.filter(*filters)
-                    .offset(params.skip)
-                    .order_by(
-                        order_by.asc() if params.asc else order_by.desc()
-                    ),
-                    (
-                        {}
-                        if params.similar_plate is None
-                        else {"similar_plate": params.similar_plate}
-                    ),
-                )
-            ).fetchall()
-
-            return [result, all_items_count]
         result = (
             await db.execute(
-                query.filter(*filters)
-                .offset(params.skip)
+                query.offset(params.skip)
                 .limit(params.limit)
-                .order_by(order_by.asc() if params.asc else order_by.desc()),
-                (
-                    {}
-                    if params.similar_plate is None
-                    else {"similar_plate": params.similar_plate}
-                ),
+                .order_by(
+                    (Record.start_time if params.sort_by == schemas.record.SortBy.entrance_time else Record.end_time)
+                    .asc() if params.asc else
+                    (Record.start_time if params.sort_by == schemas.record.SortBy.entrance_time else Record.end_time)
+                    .desc()
+                )
             )
         ).fetchall()
+    
         return [result, all_items_count]
-
+        
     async def get_record(
         self,
         db: Session | AsyncSession,
